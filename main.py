77c0d1d5684e8bcae565af4367c58132edeb3310
Arpit <arpitsengar99@gmail.com>
2023-08-28 09:51:27 +0530

    changed bg

--- main.py
+++ main.py
@@ -8,29 +8,29 @@ from tuberculosis import tuberculosis_page
 st.set_page_config(page_title="Omdena Myanmar", page_icon="🇲🇲", initial_sidebar_state="expanded")
 
 
-# def change_bg():
-#     @st.cache_data()
-#     def get_base64_of_bin_file(bin_file):
-#         with open(bin_file, 'rb') as f:
-#             data = f.read()
-#         return base64.b64encode(data).decode()
-#
-#     def set_png_as_page_bg(png_file):
-#         bin_str = get_base64_of_bin_file(png_file)
-#         page_bg_img = '''
-#         <style>
-#         .stApp {
-#         background-image: url("data:image/png;base64,%s");
-#         background-size: cover;
-#         background-repeat: no-repeat;
-#         background-attachment: scroll; # doesn't work
-#         }
-#         </style>
-#         ''' % bin_str
-#
-#         st.markdown(page_bg_img, unsafe_allow_html=True)
-#
-#     set_png_as_page_bg('assets/temp2.webp')
+def change_bg():
+    @st.cache_data()
+    def get_base64_of_bin_file(bin_file):
+        with open(bin_file, 'rb') as f:
+            data = f.read()
+        return base64.b64encode(data).decode()
+
+    def set_png_as_page_bg(png_file):
+        bin_str = get_base64_of_bin_file(png_file)
+        page_bg_img = '''
+        <style>
+        .stApp {
+        background-image: url("data:image/png;base64,%s");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: scroll; # doesn't work
+        }
+        </style>
+        ''' % bin_str
+
+        st.markdown(page_bg_img, unsafe_allow_html=True)
+
+    set_png_as_page_bg('assets/temp2.webp')
 
 
 # change_bg()
