4795a0b08174ffdf4a4c7f9aa66f7f9db6b4fed9
CareNetAI <159421601+CareNetAI@users.noreply.github.com>
2024-02-14 21:17:06 +0630

    Update config.py

--- config.py
+++ config.py
@@ -1,54 +1,32 @@
-PROJECT_BACKGROUND = """<h1>Project background</h1><br><p>In recent years, the world has witnessed the devastating impact of the COVID-19 pandemic, 
-    which has caused significant loss of life and economic disruptions across nations. As one of the most effective 
-    tools for identifying COVID-19, chest X-rays have played a crucial role in diagnosing and monitoring the 
-    progression of the disease. However, the manual interpretation of these images by healthcare professionals can be 
-    time-consuming and subjective, often leading to delays in diagnosis and potential errors.<br><br> In addition to 
-    COVID-19, there are numerous other respiratory and chest-related diseases that require accurate and timely 
-    detection, such as pneumonia, tuberculosis, and lung cancer. Early detection of these diseases is vital for 
-    effective treatment and improved patient outcomes. Unfortunately, healthcare systems in many parts of the world, 
-    including Myanmar, face resource constraints, limited access to specialized expertise, and a lack of 
-    sophisticated diagnostic tools. These challenges hinder the timely detection and appropriate management of 
-    respiratory diseases, leading to increased morbidity and mortality rates. <br><br>To address these critical 
-    issues, the Omdena Myanmar Chapter has initiated the “Identifying Diseases in Chest X-Rays & COVID-19 Detection” 
-    project. The goal is to leverage deep learning techniques to develop an automated system capable of accurately 
-    detecting COVID-19 and various chest-related diseases from radiographic images. By harnessing the power of 
-    artificial intelligence and computer vision, this project aims to revolutionize the diagnostic process and 
-    facilitate early intervention, ultimately saving lives and improving healthcare outcomes.</p><br>
-    Local Chapter Website : <a href="https://omdena.com/local-chapters/myanmar-chapter/">Visit Site</a><br>
-    Github Repository     : <a href="https://github.com/OmdenaAI/myanmar-chapter-chest-x-rays">Visit Site</a><br>
+PROJECT_BACKGROUND = """<h1>Business Model</h1><br><p>CareNet is an innovative digital healthcare platform originating from
+Myanmar, designed to leverage the transformative power of Artificial Intelligence (AI)
+and Machine Learning (ML) technologies. Our mission is to make advanced healthcare
+services—such as early disease detection, virtual health assistance, and personalized
+health monitoring—accessible to a global audience, with a special focus on underserved
+communities.</p><br>
+    Our Website : <a href="https://carenetai.github.io/">Visit Site</a><br>
+    LLM ChatBot Demo : <a href="https://llamatwoandreplicate.streamlit.app/">Visit Site</a><br>
     """
 
-PROJECT_GOALS = f"""<h2>Project goals</h2> <p>The project's primary objective is to create an AI-driven healthcare 
-    solution for chest X-ray analysis, disease detection, and COVID-19 diagnosis, utilizing deep learning algorithms. 
-    To begin, an extensive literature review will be conducted, informing the project's goals. A diverse dataset of 
-    chest X-ray images, encompassing COVID-19 cases and other chest diseases, will be collected and preprocessed for 
-    model training. Exploratory data analysis will be performed to reveal insights and potential 
-    biases.<br><br>Choosing the most appropriate deep learning algorithm or architecture will be a key step, 
-    considering factors such as complexity, interpretability, and computational requirements. The selected model will 
-    be implemented, trained, and optimized to achieve precise disease detection. Additionally, a user-friendly web or 
-    mobile app will be developed to provide real-time results for uploaded chest X-ray images.<br><br>The final 
-    solution will be deployed in a secure, scalable hosting environment. Comprehensive project documentation and a 
-    final presentation will be delivered to showcase results and the solution's potential impact on healthcare. 
-    Knowledge sharing and open discussions about AI-driven healthcare solutions will be encouraged.</p> """
+PROJECT_GOALS = f"""<h2>Mission and Vision Statement</h2> <p>To make people accessible to advanced healthcare through AI and
+ML, ensuring that every individual, regardless of their geographic or economic status,
+can receive early disease detection services and emotional support. </br> Our vision is to establish CareNet as a global supporter in digital
+healthcare, bridging the gap between advanced health technologies and the
+communities that need them the most, thereby revolutionizing healthcare access and
+quality on a worldwide scale </p> """
 
-PROJECT_PROBLEM = f"""<h2>The Problem</h2> <p>The problem we aim to solve through this project is two-fold. Firstly,
-    the lack of accessible and reliable diagnostic tools for chest diseases, including COVID-19, hampers timely
-    detection and intervention, which can lead to the rapid spread of the disease and its associated complications.
-    Secondly, the scarcity of specialized healthcare professionals, particularly radiologists, in many parts of
-    Myanmar, exacerbates the problem by limiting the availability of accurate and prompt diagnoses. <br><br>Our local
-    community faces the burden of inadequate healthcare infrastructure and limited resources, which further amplifies
-    the impact of these challenges. Early detection of COVID-19 and other chest diseases is crucial for effective
-    treatment and preventing the spread of the virus. By deploying an AI-powered solution capable of accurately
-    analyzing chest X-rays and identifying diseases, we can make a significant positive impact on the healthcare
-    landscape of Myanmar, Asian countries, and people around the world. <br><br>The deep learning model we develop will
-    enable healthcare providers, including general practitioners and healthcare workers in remote areas, to quickly
-    identify diseases in chest X-rays. By reducing the dependence on scarce human resources and improving the
-    efficiency of diagnoses, our solution will enhance the overall quality of healthcare services. Moreover,
-    the availability of a reliable and accessible diagnostic tool will empower medical professionals to make informed
-    decisions and provide timely treatments, potentially saving lives and mitigating the spread of diseases within
-    the community. **The productivity of our product will increase as much as the support we receive**, as our model
-    is greatly dependent on the support of data. <br><br>Through the development of a web app or mobile app, we aim to make
-    this solution widely accessible beyond our local community, reaching healthcare providers globally. By
-    democratizing access to advanced diagnostic capabilities, we strive to contribute to the global fight against
-    COVID-19 and other chest diseases, fostering a healthier future for individuals worldwide.<br>
+PROJECT_PROBLEM = f"""<h2>The Problem Statement</h2> <p>The healthcare sector is grappling with numerous challenges that have been
+exacerbated by the global disparity in healthcare access and the impact of recent health
+crises, such as the COVID-19 pandemic. Despite advancements in medical technology,
+significant barriers remain in early disease detection and mental health support,
+particularly in underserved and developing regions. This disparity is further widened by
+a shortage of healthcare professionals, limited access to advanced diagnostic tools,
+and the high cost of medical services. Diseases like Alzheimer's, lung cancer,
+pneumonia, tuberculosis, and COVID-19 require early detection for effective treatment,
+yet the resources for such diagnostics are unevenly distributed globally. Moreover, the
+mental health sector suffers from a lack of accessible and immediate support for
+individuals facing emotional distress, contributing to a global crisis where care is often
+reactive rather than proactive. These challenges underscore the urgent need for
+innovative solutions that democratize access to healthcare services, ensuring timely
+and equitable healthcare support for all, regardless of geographic or economic status.<br>
 """
