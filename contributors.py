158c3c45327a36aaaf8e4d9705808d9bad0fb9c7
CareNetAI <159421601+CareNetAI@users.noreply.github.com>
2024-02-14 21:10:56 +0630

    Update contributors.py

--- contributors.py
+++ contributors.py
@@ -3,7 +3,7 @@ import streamlit as st
 def contributors_page():
     st.balloons()
     st.write("""
-                <h1 style="text-align: center; color:#FFF6F4;">A heartfelt thankyou to all our contributors ❤️</h1><hr>
+                <h1 style="text-align: center; color:#FFF6F4;">A heartfelt thankyou to all our Members ❤️</h1><hr>
                 <div style="text-align:center;">
                 <table>
                 <tr>
@@ -11,7 +11,7 @@ def contributors_page():
                     <th width="20%" style="font-size: 140%;">Chapter Lead</th>    
                 </tr>
                 <tr>
-                    <td>Omdena Myanmar Chapter</td>    
+                    <td>CareNetAI</td>    
                     <td>Min Htet Myet</td>    
                 </tr>
                 </table>
@@ -30,87 +30,7 @@ def contributors_page():
                             <td>Tuberculosis Model</td>
                             <td>Alpha Lossangoyi Nanga</td>
                         </tr>
-                        <tr>
-                            <td>Pneumonia Model</td>
-                            <td>Ron de Guzman</td>
-                        </tr>
-                        <tr>
-                            <td>Deployment</td>
-                            <td>Arpit Sengar</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <br>
-                <table>
-                    <tbody>
-                        <tr>
-                            <th width="20%" style="font-size: 140%;" colspan="3">Contributors</th>
-                        </tr>
-                        <tr>
-                            <td width="20%">Alpha Lossangoyi-Nanga</td>
-                            <td width="20%">Amyr Edmar Francisco</td>
-                            <td width="20%">Aya Nabil</td>
-                        </tr>
-                        <tr>
-                            <td width="20%">Basava Chari Boppudi</td>
-                            <td width="20%">Deepak Tp</td>
-                            <td width="20%">Dheemanth Bhat</td>
-                        </tr>
-                        <tr>
-                            <td width="20%">Dinara Issagaliyeva</td>
-                            <td width="20%">Dorsa Rohani</td>
-                            <td width="20%">Emmanuel Osanebi</td>
-                        </tr>
-                        <tr>
-                            <td width="20%">Geremae Tibule</td>
-                            <td width="20%">José Zerna</td>
-                            <td width="20%">Lucy Lifschitz</td>
-                        </tr>
-                        <tr>
-                            <td width="20%">Marco Antonio Chacon Amaro</td>
-                            <td width="20%">Md. Anas Mondol Mondol</td>
-                            <td width="20%">Melania Abrahamian</td>
-                        </tr>
-                        <tr>
-                            <td width="20%">Mohsen Nabil</td>
-                            <td width="20%">Nandini Kalita</td>
-                            <td width="20%">Nurullah Sirca</td>
-                        </tr>
-                        <tr>
-                            <td width="20%">Palak Kakani</td>
-                            <td width="20%">Pranav M</td>
-                            <td width="20%">Ramya Dirisanam</td>
-                        </tr>
-                        <tr>
-                            <td width="20%">Rishabh  Mishra</td>
-                            <td width="20%">Rishik Shekar Salver</td>
-                            <td width="20%">Rohan Balaji</td>
-                        </tr>
-                        <tr>
-                            <td width="20%">Ron de Guzman</td>
-                            <td width="20%">Sai Prasanna Kumar Kumaru</td>
-                            <td width="20%">Sidharth S.R</td>
-                        </tr>
-                        <tr>
-                            <td width="20%">Siva Sai Kumar Reddy Kachana</td>
-                            <td width="20%">Srushti Ajay Pawar</td>
-                            <td width="20%">Steven Yan</td>
-                        </tr>
-                        <tr>
-                            <td width="20%">Sudhanshu  Rastogi</td>
-                            <td width="20%">Tajamul Islam</td>
-                            <td width="20%">Tamer Ahmed</td>
-                        </tr>
-                        <tr>
-                            <td width="20%">Thenuka Dharmaseelan</td>
-                            <td width="20%">Tim Hayes</td>
-                            <td width="20%">Tinotenda  Mangarai</td>
-                        </tr>
-                        <tr>
-                            <td width="20%">Arudhra Narasimhan</td>
-                            <td width="20%">Yasir Ech-Chammakhy</td>
-                            <td width="20%">Zyrolie Suare</td>
-                        </tr>
+                        
                     </tbody>
                 </table>
                 </div>
